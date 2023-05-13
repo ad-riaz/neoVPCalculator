4a982cee75b875525b90a31abfb4d7b0c7648d5d
ad-riaz <ad.riazantsev@gmail.com>
2023-05-13 18:45:10 +0300

    Add SpringDoc into project.

--- src/main/java/io/github/adriaz/neovpcalculator/controller/PaymentController.java
+++ src/main/java/io/github/adriaz/neovpcalculator/controller/PaymentController.java
@@ -2,6 +2,9 @@ package io.github.adriaz.neovpcalculator.controller;
 
 import io.github.adriaz.neovpcalculator.exception.*;
 import io.github.adriaz.neovpcalculator.service.PaymentCalculationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +13,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(
+        name = "Калькулятор отпускных",
+        description = "Содержит метод(ы) для работы с отпускными отчислениями"
+)
 public class PaymentController {
     @Autowired
     private PaymentCalculationService paymentService;
 
     @GetMapping("calculate")
-    public ResponseEntity calcVacationPayment(@RequestParam double avgSalary, @RequestParam String startDate, @RequestParam String endDate) {
+    @Operation(summary = "Позволяет вычислить сумму отпускных отчислений")
+    public ResponseEntity calcVacationPayment(@RequestParam @Parameter(description = "Средняя месячная заработная плата") double avgSalary,
+                                              @RequestParam @Parameter(description = "Дата начала отпуска в формате dd-MM-yyyy") String startDate,
+                                              @RequestParam @Parameter(description = "Дата конца отпуска в формате dd-MM-yyyy") String endDate) {
         try {
             int vacationDuration = paymentService.calcVacationDuration(startDate, endDate);
             double payment = paymentService.calcPayment(avgSalary, vacationDuration);
